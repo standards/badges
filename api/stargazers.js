3fea1a68186646bdcce26ad4617bc7414cc06755
Leo Giovanetti <hello@leog.me>
2020-06-06 22:04:47 -0300

    missing headers

--- api/stargazers.js
+++ api/stargazers.js
@@ -2,6 +2,8 @@ const github = require("octonode");
 const client = github.client(process.env.GITHUB_TOKEN);
 
 module.exports = (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Cache-Control', 's-maxage=604800');
     res.setHeader('Content-Type', 'application/json');
     client.get('/repos/standards/meta/stargazers', { per_page: 200 }, function (err, status, result, headers) {
